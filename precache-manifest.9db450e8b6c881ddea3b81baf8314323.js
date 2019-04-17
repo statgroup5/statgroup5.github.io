3404414f50510d87b271d41bcd936ba11f1520af
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-04-18 01:39:24 +0700

    test

---
+++ precache-manifest.9db450e8b6c881ddea3b81baf8314323.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1a4566f2e384e9a5c62",
+    "url": "/Visualization/static/js/runtime~main.69866bac.js"
+  },
+  {
+    "revision": "706bc88cb05024f1f455",
+    "url": "/Visualization/static/js/main.f8a985b9.chunk.js"
+  },
+  {
+    "revision": "2039f6769238f4dfd971",
+    "url": "/Visualization/static/js/2.6b1538ba.chunk.js"
+  },
+  {
+    "revision": "706bc88cb05024f1f455",
+    "url": "/Visualization/static/css/main.53e65297.chunk.css"
+  },
+  {
+    "revision": "a6e21b1e0860198f258c949a300a236b",
+    "url": "/Visualization/index.html"
+  }
+];
